3978564b4338ec0e299e68b7d246628ddb5f0e50
AronGomu <neverismine@gmail.com>
2023-12-29 13:29:07 +0100

    setting up project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<h1 class="text-3xl text-primary font-bold underline">Caloruck</h1>
+
+<label class="form-control w-full max-w-xs">
+	<div class="label">
+		<span class="label-text">Distance</span>
+	</div>
+	<input
+		type="text"
+		placeholder="Distance"
+		class="input input-bordered w-full max-w-xs"
+	/>
+</label>
+
+<label class="form-control w-full max-w-xs">
+	<div class="label">
+		<span class="label-text">Time</span>
+	</div>
+	<input
+		type="text"
+		placeholder="Time"
+		class="input input-bordered w-full max-w-xs"
+	/>
+</label>
+
+<div>
+	<div>Calorie Burned :</div>
+	<div class="text-2xl text-secondary font-bold">Caloruck</div>
+</div>
